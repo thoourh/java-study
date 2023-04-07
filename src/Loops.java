f317b1a8b762aa57c9acb93a60f0e78d057b4cd6
wongi <wongi04@naver.com>
2023-04-07 23:11:07 +0900

    Loops_2

--- src/Loops.java
+++ src/Loops.java
@@ -2,8 +2,18 @@ public class Loops {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++){
-            System.out.println(i);
-        }
+    //    for (int i = 0; i < 10; i++){
+    //        System.out.println(i);
+    //    }
+
+        int b = 10;
+ //       while (b > 0) {
+ //           System.out.println(b);
+ //           b--;
+ //       }
+        do{
+            System.out.println(b);
+            b--;
+        } while (b > 0);
     }
 }
