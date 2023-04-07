2485bf3aa6635b46c064afa2967c4bd6b76cba5b
wongi <wongi04@naver.com>
2023-04-07 21:05:48 +0900

    conditional_1

---
+++ src/conditional_java.java
@@ -0,0 +1,22 @@
+public class conditional_java {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 100;
+
+        if (a == b){
+            System.out.println("a == b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("else");
+        }
+
+        //if (a == b){
+        //    System.out.println("a == b");
+        //} else {
+        //   System.out.println("else block");
+        //}
+    }
+}
---
+++ src/dd.java
@@ -0,0 +1,11 @@
+public class dd {
+
+    public static void main(String[] args) {
+
+        int a =1;
+        double price = 12.5;
+
+        System.out.println(++a);
+        System.out.println(a);
+    }
+}
