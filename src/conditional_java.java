aded0bcad9c4499056bf1763ffd2bbf8e17d47fc
wongi <wongi04@naver.com>
2023-04-07 22:51:31 +0900

    conditional_java_2

--- src/conditional_java.java
+++ src/conditional_java.java
@@ -2,21 +2,42 @@ public class conditional_java {
 
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 100;
+        //int a = 1;
+        //int b = 100;
 
-        if (a == b){
-            System.out.println("a == b");
-        } else if (a < b) {
-            System.out.println("a < b");
-        } else {
-            System.out.println("else");
-        }
+        //if (a == b){
+        //    System.out.println("a == b");
+        //} else if (a < b) {
+        //    System.out.println("a < b");
+        //} else {
+        //    System.out.println("else");
+        //}
 
         //if (a == b){
         //    System.out.println("a == b");
         //} else {
         //   System.out.println("else block");
         //}
+        int a = 12;
+
+
+        switch (a + 1){
+
+            case 9:
+                System.out.println("a+1==9");
+                break;
+            case 10:
+                System.out.println("a+1==10");
+                break;
+            case 11:
+                System.out.println("a+1==11");
+                break;
+            case 12:
+                System.out.println("a+1==12");
+                break;
+            default:
+                System.out.println("default");
+
+        }
     }
 }
