da534c04dbf864792e372f928369b7feb7e326f6
wongi <wongi04@naver.com>
2023-04-08 23:54:20 +0900

    ArrayExample_2

---
+++ src/ArrayExample.java
@@ -0,0 +1,16 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        int[] price = {10000,9000,40000,7000};
+        String[] mbti = {"INFP", "ENFP", "ISTJ", "ESTP"};
+
+//        System.out.println(price[0]);
+//        System.out.println(mbti[0]);
+
+        price[1] = 8000;
+//        System.out.println(price[1]);
+
+        System.out.println(price);
+        
+    }
+}
