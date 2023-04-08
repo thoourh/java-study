dd5d9600bdc82620b2a7edc124251a70f5486223
wongi <wongi04@naver.com>
2023-04-08 23:44:29 +0900

    Array_1

--- src/ArrayExanple.java
+++ src/ArrayExanple.java
@@ -1,6 +1,11 @@
 public class ArrayExanple {
     public static void main(String[] args) {
 
+        int[] price = {10000,9000,40000,7000};
+        String[] mbti = {"INFP", "ENFP", "ISTJ", "ESTP"};
+
+        System.out.println(price[0]);
+        System.out.println(mbti[0]);
         
     }
 }
